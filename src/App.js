532e273c2d3b7c0d06d171a5b2669293695edcbf
Sandeep <sandipsharma2445@gmail.com>
2025-03-21 11:49:51 +0530

    Restaurant Summary added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Header from "./componets/Layout/Header";
+import Meals from "./componets/Meals/Meals";
 
 function App() {
   return (
     <>
     <Header />
+    <main>
+      <Meals />
+    </main>
     </>
   );
 }
