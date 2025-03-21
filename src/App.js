96fd1805c221bc45d64c14760df7521b92bc1e8b
Sandeep <sandipsharma2445@gmail.com>
2025-03-21 12:54:00 +0530

    Cart modal added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./componets/Layout/Header";
 import Meals from "./componets/Meals/Meals";
+import Cart from "./componets/Cart/Cart";
 
 function App() {
   return (
     <>
+    <Cart />
     <Header />
     <main>
       <Meals />
